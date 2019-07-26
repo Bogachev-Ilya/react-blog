1b61b57312e6f486344eb3793a2596f633f56bc0
Ilia Bogachev <ilia_bogachev@epam.com>
2019-07-26 15:15:52 +0300

    add Article, Footer components and move other parts from static html to react

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
-import Navbar from './components/Navbar';
-import Welcome from './components/Welcome';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import * as serviceWorker from "./serviceWorker";
+import Navbar from "./components/Navbar";
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
 
 const Home = () => {
-    return <h1>Home</h1>
-}
+  return <h1>Home</h1>;
+};
 
 const About = () => {
-    return <h1>About app</h1>
-}
+  return <h1>About app</h1>;
+};
 
 ReactDOM.render(
-    <BrowserRouter>
-      <div>
-        
-    <Navbar />
-    <Route exact path="/" component ={Welcome}/>
-    <Route path="/home" component ={Home}/>
-    <Route path="/about" component ={About}/>
+  <BrowserRouter>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Welcome} />
+      <Route path="/home" component={Home} />
+      <Route path="/about" component={About} />
+      <Footer />
     </div>
-    </BrowserRouter>,    
-    document.getElementById('root'));
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
